86e7abb6ee609648182affc3ad7cc41c62852c57
tans105 <tanmayawasthi105@gmail.com>
2016-11-12 16:04:04 +0530

    For testing Idea

---
+++ src/linkedlist/HelloWorld.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+/**
+ * Created by tanmay on 12/11/16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String echoString=returnHelloWorldString();
+        System.out.println(echoString);
+    }
+
+    private static String returnHelloWorldString() {
+        return "Hello World";
+    }
+}
+
+
